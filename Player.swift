8f45a2e6dc6e76740a0126aa7548d42072aee1fc
Massinissa75 <46651734+Massinissa75@users.noreply.github.com>
2019-03-13 19:06:35 +0100

    Add files via upload

---
+++ Player.swift
@@ -0,0 +1,46 @@
+//
+//  Player.swift
+//  Projet 03
+//
+//  Created by Massinissa_theking on 12/03/2019.
+//  Copyright © 2019 Massinissa_theking. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    
+    var name : String = ""
+    var team :[ Character ] = []
+    
+    
+    func creatTeam (){
+        print(" veuillez choisir parmis les personnages suivant !")
+        
+        switch Character {
+        case <#pattern#>:
+            <#code#>
+        default:
+            <#code#>
+        }
+        
+        // afficher les personnages existant
+        
+        func getCharacter () -> Character {
+       
+        
+        
+        // ecouter l'entree clavier pour le choix d'un personnage
+            
+            // On récupère la réponse de l'utilisateur
+            if let choice = readLine() {
+                switch choice {
+                case "1": // Ajouter une nouvelle activité
+                
+        
+        // repeter l'operation jusqu'a ce que on est 3 personnages dans l'equipe
+        
+    }
+    
+}
+}
---
+++ main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Projet 03
+//
+//  Created by Massinissa_theking on 06/03/2019.
+//  Copyright © 2019 Massinissa_theking. All rights reserved.
+//
+
+
+
+let massinissa = Wizard (name: "massinissa", life: 99 , dammage: 80, weapon: "sword")
+
+var swiftRobot = Colossus (name: "antonio", life: 100, dammage: 90, weapon: "clavier")
+
